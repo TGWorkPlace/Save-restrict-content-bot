2b50cdd7a49e471db2f6ff7846954f6fdfdb3fd9
TGWorkPlace <DramaShip.org@gmail.com>
2026-06-30 13:06:11 +0530

    Add files via upload

---
+++ cantarella/__init__.py
@@ -0,0 +1,16 @@
+import log
+from log import setup_telegram_logging, stop_telegram_logging
+# cantarella
+# Don't Remove Credit
+# Telegram Channel @cantarellabots
+
+
+# cantarella
+# Don't Remove Credit
+# Telegram Channel @cantarellabots
+
+
+
+# cantarella
+# Don't Remove Credit
+# Telegram Channel @cantarellabots
---
+++ cantarella/restart.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import asyncio
+from pyrogram import Client, filters, enums
+from pyrogram.errors import (
+    FloodWait, UserIsBlocked, InputUserDeactivated, UserAlreadyParticipant,
+    InviteHashExpired, UsernameNotOccupied, AuthKeyUnregistered, UserDeactivated, UserDeactivatedBan
+)
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery, InputMediaPhoto
+from config import API_ID, API_HASH, ERROR_MESSAGE, ADMINS
+import math
+from logger import LOGGER
+
+@Client.on_message(filters.command("restart") & filters.user(ADMINS))
+async def stop_button(bot, message):
+    msg = await bot.send_message(text="<b><i>ʙᴏᴛ ɪꜱ ʀᴇꜱᴛᴀʀᴛɪɴɢ</i></b>", chat_id=message.chat.id)       
+    await asyncio.sleep(3)
+    await msg.edit("<b><i><u>ʙᴏᴛ ɪꜱ ʀᴇꜱᴛᴀʀᴛᴇᴅ</u> ✅</i></b>")
+    os.execl(sys.executable, sys.executable, *sys.argv)
